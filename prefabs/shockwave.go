dd340631512fdf038cc5623f50669cc2c55fa7d3
巨龙之路 <648813609@qq.com>
2026-02-07 12:10:34 +0800

    add shockwave VFX

---
+++ prefabs/shockwave.go
@@ -0,0 +1,15 @@
+components {
+  id: "shockwave"
+  component: "/scripts/shockwave.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"circle_01\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/images/images.atlas\"\n"
+  "}\n"
+  ""
+}
